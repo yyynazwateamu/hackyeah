7201554aa383611c00dd4ed8cb743f49caa0bf64
Szymon Cader <szymon.SC.cader@gmail.com>
2019-09-13 20:04:04 +0200

    Testing ssh

--- tests.py
+++ tests.py
@@ -3,6 +3,6 @@ from math import sqrt
 
 import pytest
 
-@pytest.mark.parametrize('x, y', [(i, i*i) for i in range(10000)])
+@pytest.mark.parametrize('x, y', [(i, i*i) for i in range(100)])
 def test_which_fails(x, y):
     assert sqrt(y) == x
