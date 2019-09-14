df5a7605f6905145a05982f1a67211803a7708be
jczuchra <czuchra.jakub@gmail.com>
2019-09-14 17:22:14 +0200

    Add material-ui/icons/ lobby view

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SplashContainer from './screens/splash/SplashContainer';
 import StartContainer from './screens/start/StartContainer';
+import LobbyContainer from './screens/Lobby/LobbyContainer';
 
 const routes =
     <Router>
-        <Route exact path='/' component={SplashContainer} />
-        <Route path='/start' component={StartContainer} />
+      <Route exact path='/' component={SplashContainer} />
+      <Route path='/start' component={StartContainer} />
+      <Route path='/lobby' component={LobbyContainer} />
     </Router>;
 
 export default routes;
