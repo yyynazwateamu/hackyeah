42aa753e2c8c19e1ea7337061dfc3315d1ccf26c
Szymon Cader <szymon.SC.cader@gmail.com>
2019-09-15 01:17:09 +0200

    add setting status for user

--- accounts/models.py
+++ accounts/models.py
@@ -4,18 +4,17 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 
+
 def validate_ticket_number(ticket_number):
     train_type, number = re.match(r"([a-zA-Z]+)(\d+)", ticket_number).groups()
     if len(train_type) not in (2, 3) or len(number) < 9:
         raise ValidationError('Provide valid ticket number')
 
+
 class CustomUser(AbstractUser):
     anonymous = models.BooleanField(default=False)
     ticket_number = models.CharField(max_length=20, validators=[validate_ticket_number], null=True, blank=True)
-
-    # class UserProfile(models.Model):
-#     user = models.OneToOneField('CustomUser', on_delete=models.CASCADE)
-#     type = models.CharField(max_length=10)
+    ready = models.BooleanField(default=False)
 
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from djoser.serializers import UserCreateSerializer
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework import viewsets
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from accounts.models import CustomUser, validate_ticket_number
@@ -31,11 +32,12 @@ class AnonymousUserViewSet(APIView):
             return JsonResponse(serializer.errors, status=400)
 
 
-class UserLobbyView(APIView):
+class UserLobbyView(viewsets.GenericViewSet):
 
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
+    @action(methods=['POST',], detail=False)
+    def ticket(self, request):
         try:
             ticket_number = request.data['ticket_number']
             validate_ticket_number(ticket_number)
@@ -43,5 +45,13 @@ class UserLobbyView(APIView):
             return JsonResponse({"ticket_number": ['Provide valid ticket number',]}, status=400)
         user = request.user
         user.ticket_number = ticket_number
+        user.save()
+        return JsonResponse(UserSerializer(request.user).data)
+
+    @action(methods=['POST', ], detail=False)
+    def status(self, request):
+        ready = request.data.get('ready', False)
+        user = request.user
+        user.ready = ready
         user.save()
         return JsonResponse(UserSerializer(request.user).data)
