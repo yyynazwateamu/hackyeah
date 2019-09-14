a9153ba4273222f3a721a473694054d35f970f97
krzempekk <kamilkrzempek99@gmail.com>
2019-09-14 21:26:20 +0200

    Quick fix for auth reducer

--- frontend/src/shared/reducers/auth.reducer.js
+++ frontend/src/shared/reducers/auth.reducer.js
@@ -15,14 +15,14 @@ export function authReducer(state = initialState, action) {
 	case authConstants.LOGIN_WITH_ACCOUNT_SUCCESS:
 		return { state, status: requestStatus.SUCCESS, token: action.payload.token, anonymous: false };
 	case authConstants.LOGIN_WITH_ACCOUNT_FAILURE:
-		return { state, status: requestStatus.ERROR, error: action.payload.error };
+		return { state, status: requestStatus.ERROR, error: action.error };
 
 	case authConstants.LOGIN_WITHOUT_ACCOUNT_REQUEST:
 		return { ...state, status: requestStatus.PENDING };
 	case authConstants.LOGIN_WITHOUT_ACCOUNT_SUCCESS:
 		return { ...state, status: requestStatus.SUCCESS, token: action.payload.token, anonymous: true };
 	case authConstants.LOGIN_WITHOUT_ACCOUNT_FAILURE:
-		return { ...state, status: requestStatus.FAILURE, error: action.payload.error };
+		return { ...state, status: requestStatus.FAILURE, error: action.error };
 
 	case authConstants.LOGOUT:
 		return initialState;
