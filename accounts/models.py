9e0a73682ac7a08725a48905a028b404a4a9b021
Szymon Cader <szymon.SC.cader@gmail.com>
2019-09-14 15:50:20 +0200

    Add authentication

--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,11 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    # class Meta:
-    #     proxy = True
-    def say_hello(self):
-        print(CustomUser.say_hello, 'accounts model')
-        return "hello"
+    pass
 
-class UserProfile(models.Model):
-    user = models.OneToOneField('CustomUser', on_delete=models.CASCADE)
-    type = models.CharField(max_length=10)
+# class UserProfile(models.Model):
+#     user = models.OneToOneField('CustomUser', on_delete=models.CASCADE)
+#     type = models.CharField(max_length=10)
 
 
 
