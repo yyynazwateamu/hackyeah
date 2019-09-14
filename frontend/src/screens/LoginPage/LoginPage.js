fefb614f9e05bb8499c1be9a982750179ed8731d
MikeyZat <mikeyzat@gmail.com>
2019-09-14 21:08:08 +0200

    Implement view of login page

--- frontend/src/screens/LoginPage/LoginPage.js
+++ frontend/src/screens/LoginPage/LoginPage.js
@@ -1,13 +1,131 @@
 //@flow
-import React from 'react';
+import React, {useState} from 'react';
+import { connect } from 'react-redux';
+import {
+  Container, Typography,
+  CssBaseline, TextField, Button, Grid, Link
 
-const LoginPage = () => {
+} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles(theme => ({
+  formContainer: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  header: {
+    textAlign: 'center',
+    width:'70%',
+    borderBottom: '1px solid #aaa',
+    paddingBottom: '5px',
+  },
+
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  register: {
+    textAlign: 'right',
+  }
+}));
+
+const LoginPage = (props: Props) => {
+
+  const classes = useStyles();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+  };
 
   return (
-    <div>
-    </div>
+    <React.Fragment>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <div className={classes.formContainer} >
+          <Typography component="h1" variant="h4" className={classes.header} >
+            ZALOGUJ SIĘ
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="filled"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Nazwa użytkownika"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              aria-describedby="username-helper-text"
+            />
+            <TextField
+              variant="filled"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Hasło"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              aria-describedby="password-helper-text"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              size="large"
+              className={classes.submit}
+            >
+              Zaloguj się
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Zapomniałeś hasła?
+                </Link>
+              </Grid>
+              <Grid item xs className={classes.register}>
+                <Link href="#" variant="body2">
+                  Nie masz jeszcze konta? Zarejestruj się!
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </React.Fragment>
   );
 };
 
+type Props = {
 
-export default LoginPage;
+};
+
+const mapStateToProps = (state) => ({
+
+});
+
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
