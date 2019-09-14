5e665dfa5f57fde133b57ca59aa68fced9e2c7f5
krzempekk <kamilkrzempek99@gmail.com>
2019-09-14 20:55:29 +0200

    Added error handling for auth-actions and logout action

--- frontend/src/shared/services/auth.service.js
+++ frontend/src/shared/services/auth.service.js
@@ -3,30 +3,35 @@ import { fetchWrapper, JWTUtils } from '@util';
 const loginWithAccount = (username, password) => {
 	return fetchWrapper.POST('/accounts/jwt/create/', { username, password })
 		.then(response => {
-			console.log(response);
 			const JWT = response.data.access;
 			JWTUtils.setJWT(JWT);
 			return JWT;
 		})
 		.catch(error => {
-			throw error;
+			const { response: { data: { detail }, status: code } } = error;
+			throw { code, detail };
 		});
 };
 
 const loginWithoutAccount = username => {
 	return fetchWrapper.POST('/accounts/anonymous/login/', { username })
 		.then(response => {
-			console.log(response);
 			const JWT = response.data.access;
 			JWTUtils.setJWT(JWT);
 			return JWT;
 		})
 		.catch(error => {
-			throw error;
+			const { response: { data: { username: [ detail ] }, status: code } } = error;
+			throw { code, detail };
 		});
+};
+
+const logout = () => {
+	JWTUtils.deleteJWT();
 };
 
 export const authService = {
 	loginWithAccount,
-	loginWithoutAccount
+	loginWithoutAccount,
+	logout,
 };
