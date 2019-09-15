746340672304b1075f2d240b3407489046b2325a
MikeyZat <mikeyzat@gmail.com>
2019-09-15 09:29:33 +0200

    Add logged in & unathorized pages

--- frontend/src/shared/util/GuestScreen/GuestScreen.js
+++ frontend/src/shared/util/GuestScreen/GuestScreen.js
@@ -1,9 +1,14 @@
 //@flow
 import React, {useEffect} from 'react';
+import {Container, Typography} from '@material-ui/core';
+import {useErrorStyles, useLoginStyles} from '@util/styles';
+import CheckIcon from '@material-ui/icons/Check';
 
 let redirect = null;
 
 const GuestScreen = (props: Props) => {
+  const loginStyles = useLoginStyles();
+  const errorStyles = useErrorStyles();
 
   useEffect(() => {
     redirect = setTimeout(() => props.history.push('/lobby'),
@@ -15,9 +20,17 @@ const GuestScreen = (props: Props) => {
   }, []);
 
   return (
-    <div>
-      Jesteś już zalogowany. Zostaniesz przeniesiony do strony głównej.
-    </div>
+    <Container component="main" maxWidth="xs" >
+      <header className="header">
+        <CheckIcon className={errorStyles.loggedInIcon} fontSize="large"/>
+        <Typography component="h1" variant="h4" className={loginStyles.header}>
+          You are logged in
+        </Typography>
+        <Typography component="span" variant="span" className={loginStyles.subheader}>
+          You will be redirected to the lobby page...
+        </Typography>
+      </header>
+    </Container>
   );
 };
 
