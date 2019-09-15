c24070c9e76fc81bfaf5b5a49afe7b2fbc6edcac
MikeyZat <mikeyzat@gmail.com>
2019-09-15 08:50:58 +0200

    Change color and fix bug

--- frontend/src/shared/reducers/index.js
+++ frontend/src/shared/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './auth.reducer';
 import { questionsReducer } from './questions.reducer.js';
-export { authSelectors } from './auth.reducer';
-export { questionSelectors } from './questions.reducer';
 import { lobbyReducer } from './lobby.reducer';
 import { ticketReducer } from './ticket.reducer';
 import { userReducer } from './user.reducer';
@@ -17,6 +15,7 @@ export default combineReducers({
 
 
 export { authSelectors } from './auth.reducer';
+export { questionSelectors } from './questions.reducer';
 export { lobbySelectors } from './lobby.reducer';
 export { ticketSelectors } from './ticket.reducer';
 export { userSelectors } from './user.reducer';
