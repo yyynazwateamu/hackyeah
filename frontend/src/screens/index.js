9974c3e12ec16ec276c94968b51d7a3ce6b63d6a
MikeyZat <mikeyzat@gmail.com>
2019-09-15 11:32:33 +0200

    Implement splash page

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -2,7 +2,7 @@ export LoginPage from './LoginPage/LoginPage';
 export LoginPageGuest from './LoginPageGuest/LoginPageGuest';
 export LoginPageSplash from './LoginPageSplash/LoginPageSplash';
 export SignUp from './SignUp/SignUp';
-export SplashContainer from './splash/SplashContainer';
+export SplashPage from './SplashPage/SplashPage';
 export LobbyContainer from './Lobby/LobbyContainer';
 export QuestionContainer from './QuestionPage/QuestionContainer';
 export TicketPage from './TicketPage/TicketPage';
